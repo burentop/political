acf97d177f35fadda1e705b6e13e9c3772a4b4d2
Brent Perry <burentoperii@gmail.com>
2016-12-14 10:42:16 -0600

    add form validation to name field.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,31 @@
 $(document).ready(function () {
   $(".profile form").submit(function (event) {
     event.preventDefault();
+    var name = $("#name").val();
     var gender = parseInt($("#gender").val());
     var age = parseInt($("#age").val());
-    var color = parseInt($("#color").val());
+    var justice = parseInt($("#justice").val());
     
-    if (age < 18) {
-      $("#celeb-name").text("Homework");
-      $("#celeb-reason").text("You should really just be focused on school right now!");
-    } else if (color === 3) {
-      $("#celeb-name").text("Pee Wee Herman");
-      $("#celeb-reason").text("Pink Camo is a very weird color - so Pee Wee seems like a valid choice.")
-    } else if (gender === 0) {
-      $("#celeb-name").text("Jennifer Aniston");
-      $("#celeb-reason").text("All guys like Jennifer, right?");
+    if ((!name)) {
+      $(".input-name").addClass("has-error");
+      $("#name-warning").text("Please enter your name.");
     } else {
-      $("#celeb-name").text("Chris Pratt");
-      $("#celeb-reason").text("I hear Chris is all the rage these days.")
-    }
+      if (age < 18) {
+        $("#political-name").text("Homework");
+        $("#political-reason").text("You should really just be focused on school right now!");
+      } else if (color === 3) {
+        $("#political-name").text("Pee Wee Herman");
+        $("#political-reason").text("Pink Camo is a very weird color - so Pee Wee seems like a valid choice.")
+      } else if (gender === 0) {
+        $("#political-name").text("Jennifer Aniston");
+        $("#political-reason").text("All guys like Jennifer, right?");
+      } else {
+        $("#political-name").text("Chris Pratt");
+        $("#political-reason").text("I hear Chris is all the rage these days.")
+      }
 
-    $(".results").show();
+      $(".results").show();
+    }
+    
   });
 });
